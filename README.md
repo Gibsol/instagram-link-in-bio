82c286a010355987901415e6f4a7eff0b01214be
Gibsol <gibsol75@gmail.com>
2022-09-27 19:20:49 +0300

    created the project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# instagram-link-in-bio
-My version of the "Link in Bio" type websites.
+# "Instagram Link in Bio website" 
+My version of the "Link in Bio" type websites. And also a minimalistic template of one.
+
+## Project Status
+
+Ready to use.
+
+## Licence :w
+
+GNU General Public Licence v3.0.
---
+++ src/main.js
@@ -0,0 +1,20 @@
+linkWebsite= "https://gibsol.dev";
+linkGithub = "https://github.com/gibsol";
+linkSoundcloud = "https://soundcloud.com/gibsol";
+linkDiscord = "https://discord.com/users/247422696038596609";
+
+function forwardWebsite() {
+    location.href = websiteLink;
+}
+
+function forwardGithub() {
+    location.href = linkGithub;
+}
+
+function forwardSoundcloud() {
+    location.href = linkSoundcloud;
+}
+
+function forwardDiscord() {
+    location.href = linkDiscord;
+}
