7ad31787cf8e9e1045252b517227478ab3ede4f4
Gibsol <gibsol75@gmail.com>
2022-10-04 18:38:52 +0300

    refactored the js

--- src/main.js
+++ src/main.js
@@ -3,18 +3,6 @@ let linkGithub = "https://github.com/gibsol";
 let linkSoundcloud = "https://soundcloud.com/gibsol";
 let linkDiscord = "https://discord.com/users/247422696038596609";
 
-function forwardWebsite() {
-    location.href = linkWebsite;
-}
-
-function forwardGithub() {
-    location.href = linkGithub;
-}
-
-function forwardSoundcloud() {
-    location.href = linkSoundcloud;
-}
-
-function forwardDiscord() {
-    location.href = linkDiscord;
+function forward(link) {
+    location.href = link;
 }
